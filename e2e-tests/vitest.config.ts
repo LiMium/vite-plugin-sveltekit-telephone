0bd6f3e200831d53a18f9c1e77c0293b43939f5a
hrj <hrj@users.noreply.github.com>
2025-07-15 17:03:21 +0000

    increase vitest timeout

--- e2e-tests/vitest.config.ts
+++ e2e-tests/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    hookTimeout: 20000,
+    hookTimeout: 40000,
     setupFiles: ['./vitest.setup.ts'],
   },
 })
